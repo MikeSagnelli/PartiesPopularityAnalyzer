854b4aa38aa1d5f0c7f6fe8c11da7b7e7c690dc2
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-04-30 01:59:59 -0500

    Sign up form front-end done.

--- routes.py
+++ routes.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
+from forms import SignupForm
+import os
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+
+    if request.method == 'POST':
+        return "Success!"
+    
+    elif request.method == 'GET':
+        return render_template("signup.html", form=form)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
