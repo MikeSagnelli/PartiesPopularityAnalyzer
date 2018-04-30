99fc44ac0e5919189cb4df2b27634cba171c8041
Mike Sagnelli <ing.sagnelli@gmail.com>
2018-04-30 02:52:48 -0500

    Finished register

--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(FlaskForm):
-    first_name = StringField('First Name')
-    last_name = StringField('Last Name')
-    email = StringField('Email')
-    password = PasswordField('Password')
+    first_name = StringField('First Name', validators=[DataRequired("Please enter your first name.")])
+    last_name = StringField('Last Name', validators=[DataRequired("Please enter your last name.")])
+    email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter a valid email address.")])
+    password = PasswordField('Password', validators=[DataRequired("Please enter your password."), Length(min=6, message="Passwords must be at least 6 characters.")])
     submit = SubmitField('Sign Up')
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 from forms import SignupForm
+from models import User
 import os
 
 app = Flask(__name__)
@@ -8,21 +9,32 @@ app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
 
-app.secret_key = "development-key"
+app.secret_key = 'development-key'
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-@app.route("/signup", methods=['GET', 'POST'])
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
 
     if request.method == 'POST':
-        return "Success!"
+        if form.validate() == False:
+            return render_template('signup.html', form=form)
+        else:
+            users = mongo.db.users
+            user = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            users.insert({
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "email": user.email,
+                "pwd_hash": user.pwd_hash
+            })
+            return 'Success!'
     
     elif request.method == 'GET':
-        return render_template("signup.html", form=form)
+        return render_template('signup.html', form=form)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True) 
