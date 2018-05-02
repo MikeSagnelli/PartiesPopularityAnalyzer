79e5b5feee23c65154b1c307529b42fe7aff2a66
MikeSagnelli <ing.sagnelli@gmail.com>
2018-05-02 03:51:20 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,9 +2,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, RadioField
 from wtforms.validators import DataRequired, Email, Length
-import sys  
-
-sys.setdefaultencoding('utf8')
 
 class SignupForm(FlaskForm):
     first_name = StringField('First Name', validators=[DataRequired("Please enter your first name.")])
